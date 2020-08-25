d9e3f6ae9a7d06c2f8ea23249905232872b09c3b
taiga <taiga.similarities@gmail.com>
2020-08-25 13:01:11 +0900

    Changed href link in a Footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="https://twitter.com/taiga88110899" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://twitter.com/taiga_coding" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="https://www.instagram.com/taigaaa_english/?hl=ja" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://www.instagram.com/taigaaa_life/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="https://github.com/taiga-programming" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                 <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
